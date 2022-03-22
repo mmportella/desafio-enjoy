fd5cbb6092537faf42f817249eddd1ed38a60468
Marcelo Martins Portella Filho <m.portella.filho@avanade.com>
2022-03-21 22:45:21 -0300

    Concluídos os métodos de ticket médio e de frequência

--- src/main/java/br/com/fiap/enjoy/test/Teste.java
+++ src/main/java/br/com/fiap/enjoy/test/Teste.java
@@ -1,13 +1,15 @@
 package br.com.fiap.enjoy.test;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
@@ -24,14 +26,22 @@ public class Teste {
 		int telefoneUsuario = 119123456;
 		
 		String ultimaVisita = ultimaVisita(telefoneUsuario);
-		System.out.println(ultimaVisita);
+		System.out.println("A data da última visita do cliente foi em " + ultimaVisita);
 		
 		Preferida preferida = estiloBebida(telefoneUsuario);
-		System.out.println("Sua bebida preferida é a " + preferida.getBebida() +
+		System.out.println("A bebida preferida de " + telefoneUsuario + " é a " + preferida.getBebida() +
 							", uma bebida do tipo " + preferida.getTipo() +
-							", e você a bebeu " + preferida.getQuantidade() +
+							", e foi consumida " + preferida.getQuantidade() +
 							" vezes.");
 		
+		
+		String frequencia = frequencia(telefoneUsuario);
+		System.out.println(frequencia);
+		
+		int cdEstabelecimento = 1;
+		
+		String ticketMedio = ticketMedio(cdEstabelecimento);
+		System.out.println("O ticket médio do estabelecimento " + cdEstabelecimento + " foi de R$" + ticketMedio);
 		
 	}
 	
@@ -45,29 +55,73 @@ public class Teste {
 		EntityManager em = Persistence.createEntityManagerFactory("desafio-enjoy").createEntityManager();
 		
 		Query q = em.createQuery("from Visita where n_telefone = :telefone order by dt_visita DESC");
+		
 		q.setParameter("telefone", telefone);
 		q.setMaxResults(1);
 		List<Visita> results = q.getResultList();
 		em.close();
 		
-		return results.get(0).getDataVisita().toString();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		String date = sdf.format(results.get(0).getDataVisita().getTime());
+		return date;
+		
 	}
 
+	
+	
 	// frequência (quantidade de visitas pelo total de dias)
-	//private static String frequencia(int telefone) {
+	private static String frequencia(int telefone) {
 		
+		EntityManager em = Persistence.createEntityManagerFactory("desafio-enjoy").createEntityManager();
 		
-	//}
+		Query q = em.createQuery("from Visita where n_telefone = :telefone");
+		q.setParameter("telefone", telefone);
+		q.setMaxResults(100);
+		List<Visita> lista = q.getResultList();
+
+		int i = lista.size();
+		
+		LocalDate hoje = LocalDate.now();
+		LocalDate primeiraVisita = LocalDate.ofInstant(lista.get(0).getDataVisita().toInstant(), ZoneId.systemDefault());
+		
+		long p = ChronoUnit.DAYS.between(primeiraVisita, hoje);
+		
+		String str = String.format("A frequência de visitas do cliente foi de " +
+				"%d vezes nos últimos %d dias", i, p);
+		
+		return str;
+		
+	}
+	
+	
 	
 	// ticket médio - 
-	//private static String ticketMedio() {
+	private static String ticketMedio(int cdEstabelecimento) {
 		
-	//}
+		EntityManager em = Persistence.createEntityManagerFactory("desafio-enjoy").createEntityManager();
+		
+		Query q = em.createQuery("from Visita where cd_estabelecimento = :cd");
+		q.setParameter("cd", cdEstabelecimento);
+		List<Visita> lista = q.getResultList();
+		
+		int visitas = lista.size();
+		float ticket = 0.0f;
+		
+		for (Visita v : lista) {
+			ticket = ticket + v.getValorTicket();
+		}
+		
+		float ticketMedio = ticket / visitas;
+		
+		String str = Float.toString(ticketMedio);
+		
+		return str;
+		
+	}
 	
 	
 	
 	// Bebida preferida, estilo e quantidade
-	
 	private static class Preferida {
 	    private String bebida;
 	    private String quantidade;
@@ -93,12 +147,11 @@ public class Teste {
 	}
 	
 	private static Preferida estiloBebida(int telefone) {
-	
-		String tel = String.valueOf(telefone); 
+	 
 		EntityManager em = Persistence.createEntityManagerFactory("desafio-enjoy").createEntityManager();
 		
 		Query q = em.createQuery("from Visita where n_telefone = :telefone order by dt_visita DESC");
-		q.setParameter("telefone", tel);
+		q.setParameter("telefone", telefone);
 		q.setMaxResults(10);
 		List<Visita> results = q.getResultList();
 		
